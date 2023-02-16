56ebd3a0eb1b6072d45dbe0fdc1122ceac0475ab
gale74 <106661600+gale74@users.noreply.github.com>
2023-02-16 11:11:18 +0100

    Delete GAgency-gotovo/GAgency-main directory

--- GAgency-gotovo/GAgency-main/menu.js
+++
@@ -1,8 +0,0 @@
-const menu = document.querySelector('.menu')
-const navbar = document.querySelector('.navItems')
-
-menu.addEventListener('click', () => {
-  navbar.classList.toggle('active')
-  menu.classList.toggle('active')
-  document.body.classList.toggle('overflow-hidden')
-})
